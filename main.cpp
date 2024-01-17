6073a3e3bf8253d9411898f2b1c333a3eaf7518d
Eckmanj8966 <124816827+Eckmanj8966@users.noreply.github.com>
2024-01-17 11:57:21 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  int y;
+  int z;
+  
+  cout << "Calclator: Addition\n";
+  
+  cout << "Enter first number: ";
+  cin >> x;
+
+  cout << "Enter second number: ";
+  cin >> y;
+
+  z = x + y;
+
+  cout << x << " + " << y << " = " << z;
+    
+}
